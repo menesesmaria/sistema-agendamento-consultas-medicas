bd77ee0b3ce63a1613ee2b268628d82d24709658
dudaeses <me157282@gmail.com>
2025-05-17 19:47:39 -0300

    criação do form de cadastro de pacientes

--- sistema_agendamento/app_sistema_agendamento/views.py
+++ sistema_agendamento/app_sistema_agendamento/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def agendar(request):
+    return render(request, 'agendar.html')
+
+def cadastrar(request):
+    return render(request, 'cadastrar.html')
--- sistema_agendamento/sistema_agendamento/urls.py
+++ sistema_agendamento/sistema_agendamento/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.contrib import admin
 from django.urls import path
 from app_sistema_agendamento import views
 
 urlpatterns = [
-    path('',views.index,name='index'),
+    path('admin/', admin.site.urls),
+    path('',views.index, name='index'),
+    path('agendar/', views.agendar, name='agendar'),
+    path('cadastrar/', views.cadastrar, name='cadastrar'),
 ]
