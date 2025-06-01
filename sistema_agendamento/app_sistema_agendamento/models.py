b921460fdea881088aa2be3ac50023e850104d5c
dudaeses <me157282@gmail.com>
2025-06-01 15:40:08 -0300

    adição da coluna CRM na tabela de profissionais

--- sistema_agendamento/app_sistema_agendamento/models.py
+++ sistema_agendamento/app_sistema_agendamento/models.py
@@ -46,9 +46,10 @@ class Profissional(models.Model):
     numero_residencia = models.IntegerField()
     cep = models.CharField(max_length=8)
     cpf = models.CharField(max_length=20)
+    crm = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
-        return f"{self.nome} - {self.especialidade_medica}"
+        return f"{self.nome} - {self.especialidade_medica} - CRM: {self.crm}"
 
 class Disponibilidade(models.Model):
     profissional = models.ForeignKey(Profissional, on_delete=models.CASCADE)
