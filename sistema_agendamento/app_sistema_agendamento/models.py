eba8a82d2dace9c60b73af7b45e64568ed943f4e
Maria Eduarda Meneses <me157282@gmail.com>
2025-07-26 17:14:49 -0300

    melhoria de relacionamentos

--- sistema_agendamento/app_sistema_agendamento/models.py
+++ sistema_agendamento/app_sistema_agendamento/models.py
@@ -15,32 +15,51 @@ class PlanoSaude(models.Model):
     def __str__(self):
         return self.nome 
 
+class Sexo(models.TextChoices):
+    masculino = 'Masculino'
+    feminino = 'Feminino'
+    outro = 'Outro'
+
+class Situacao(models.TextChoices):
+    emergencia = 'Emergência - Atendimento imediato'
+    muito_urgente = 'Muito urgente - Atendimento em até 10 minutos'
+    urgente = 'Urgente - Atendimento em até 60 minutos'
+    pouco_urgente = 'Pouco urgente - Atendimento em até 2 horas'
+    nao_urgente = 'Não urgente - Atendimento em até 4 horas'
+
 class Paciente(models.Model):
-    nome = models.CharField(max_length=100, null=True, blank=True)
-    telefone = models.CharField(max_length=15, null=True, blank=True)
-    email = models.EmailField(max_length=100, null=True, blank=True)
-    cpf = models.CharField(max_length=14, null=True, blank=True)
-    data_nascimento = models.DateField(null=True, blank=True)
-    obs_medicas = models.TextField(null=True, blank=True)
+    nome = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=15)
+    email = models.EmailField(max_length=100)
+    cpf = models.CharField(max_length=14)
+    data_nascimento = models.DateField
+    obs_medicas = models.TextField
     bairro = models.CharField(max_length=30)
     rua = models.CharField(max_length=30)
     numero_residencia = models.IntegerField()
     cep = models.CharField(max_length=8)
-    sexo = models.CharField(max_length=5)
+    sexo = models.CharField(
+        max_length=10,
+        choices=Sexo.choices,
+    )
 
     def __str__(self):
         return f"{self.nome} - CPF: {self.cpf}"
 
 class Profissional(models.Model):
     nome = models.CharField(max_length=100, null=True, blank=True)
-    sexo = models.CharField(max_length=5, null=True, blank=True)
+    sexo = models.CharField(
+        max_length=10,
+        choices=Sexo.choices,
+    )
     data_nascimento = models.DateField(null=True, blank=True)
     telefone = models.CharField(max_length=15, null=True, blank=True)
     email = models.EmailField(max_length=100, null=True, blank=True)
     tempo_experiencia = models.CharField(max_length=15, null=True, blank=True)
     nivel = models.CharField(max_length=50, null=True, blank=True)
     instituicao_formacao = models.CharField(max_length=100, null=True, blank=True)
-    especialidade_medica = models.ForeignKey(Especialidade, on_delete=models.SET_NULL, null=True)
+    especialidade_medica1 = models.ForeignKey(Especialidade, on_delete=models.SET_NULL, null=True)
+    especialidade_medica2 = models.ForeignKey(Especialidade, on_delete=models.SET_NULL, null=True)
     bairro = models.CharField(max_length=30)
     rua = models.CharField(max_length=30)
     numero_residencia = models.IntegerField()
@@ -49,7 +68,7 @@ class Profissional(models.Model):
     crm = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
-        return f"{self.nome} - {self.especialidade_medica} - CRM: {self.crm}"
+        return f"{self.nome} - {self.especialidade_medica1} - CRM: {self.crm}"
 
 class Disponibilidade(models.Model):
     profissional = models.ForeignKey(Profissional, on_delete=models.CASCADE)
@@ -65,9 +84,11 @@ class Disponibilidade(models.Model):
 class Consulta(models.Model):
     paciente = models.ForeignKey(Paciente, on_delete=models.SET_NULL, null=True)
     profissional = models.ForeignKey(Profissional, on_delete=models.SET_NULL, null=True)
-    especialidade = models.ForeignKey(Especialidade, on_delete=models.SET_NULL, null=True)
     disponibilidade = models.ForeignKey(Disponibilidade, on_delete=models.SET_NULL, null=True, blank=True)
-    situacao = models.CharField(max_length=50, null=True, blank=True)
+    situacao = models.CharField(
+        max_length=45,
+        choices=Situacao.choices,
+    )
     pagamento = models.CharField(max_length=50, null=True, blank=True)
     plano_saude = models.ForeignKey(PlanoSaude, on_delete=models.SET_NULL, null=True)
     descricao = models.TextField(null=True, blank=True)
